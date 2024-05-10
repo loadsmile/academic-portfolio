1c89cdaa199fa7d99a4cf5722eeb42d326892bf8
mariana <marianavaladares@gmail.com>
2024-05-10 12:01:20 +0100

    Added active class to LinkButton.

--- academic-portfolio/src/components/NavBar/NavBar.jsx
+++ academic-portfolio/src/components/NavBar/NavBar.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
 const NavBar = () => {
+  // This function determines the class for NavLink based on its active state
+  const getNavLinkClass = (isActive) => {
+    return isActive ? `${styles.linkButton} ${styles.linkButtonActive}` : styles.linkButton;
+  };
+
   return (
     <nav className={styles.navBar}>
       <div>
-        <h1 className={styles.navbarTitle}><Link to="/main">Susana Barradas</Link></h1>
+        <h1 className={styles.navbarTitle}><NavLink to="/main">Susana Barradas</NavLink></h1>
       </div>
       <ul className={styles.navbarList}>
         <li className={styles.navbarLink}>
-          <Link to="/research" className={styles.linkButton}>Research</Link>
+          <NavLink to="/research" className={({ isActive }) => getNavLinkClass(isActive)}>
+            Research
+          </NavLink>
         </li>
         <li className={styles.navbarLink}>
-          <Link to="/teaching" className={styles.linkButton}>Teaching</Link>
+          <NavLink to="/teaching" className={({ isActive }) => getNavLinkClass(isActive)}>
+            Teaching
+          </NavLink>
         </li>
         <li className={styles.navbarLink}>
-          <Link to="/publications" className={styles.linkButton}>Publications</Link>
+          <NavLink to="/publications" className={({ isActive }) => getNavLinkClass(isActive)}>
+            Publications
+          </NavLink>
         </li>
         <li className={styles.navbarLink}>
-          <Link to="/contact" className={styles.linkButton}>Contact</Link>
+          <NavLink to="/contact" className={({ isActive }) => getNavLinkClass(isActive)}>
+            Contact
+          </NavLink>
         </li>
       </ul>
     </nav>
