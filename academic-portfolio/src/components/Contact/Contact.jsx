5d6e6810591867757123c60e19303dd34a4c23f6
mariana <marianavaladares@gmail.com>
2024-05-10 18:08:27 +0100

    Added contact and its css module. Added some basic info and style.

--- academic-portfolio/src/components/Contact/Contact.jsx
+++ academic-portfolio/src/components/Contact/Contact.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import styles from './Contact.module.css';
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact Me</h1>
-      <p>
-        Email: <a href="mailto:Susana@gmail.com">Susana@gmail.com</a>
-      </p>
+    <div className={styles.contactContainer}>
+      <div className={styles.contactInfo}>
+        <h1 className={styles.mainTitle}>Contact me:</h1>
+        <div className={styles.email}>
+          <p>
+            <a href="mailto:susana.barradas@uexternado.edu.co">susana.barradas@uexternado.edu.co</a>
+          </p>
+        </div>
+        <div className={styles.linkedin}>
+          <p>
+            <a href="https://www.linkedin.com/in/susanabarradas/">LinkedIn</a>
+          </p>
+        </div>
+        <div className={styles.twitter}>
+          <p>
+            <a href="https://www.x.com">X</a>
+          </p>
+        </div>
+      </div>
+      <div className={styles.pictureDiv}>
+        <img src="https://www.uexternado.edu.co/wp-content/uploads/2022/04/Prf-Susana-Pataco-Zezere.jpg" alt="Susana Barradas" className={styles.profilePic}/>
+      </div>
     </div>
   );
 }
